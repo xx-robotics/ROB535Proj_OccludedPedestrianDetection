4113ff12b61f536cb51216289ca86543ac357424
Xu <hsiangxu@umich.edu>
2024-12-05 09:32:37 -0500

    runable

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11s_person.yaml")  # build a new model from YAML
+model = YOLO("yolo11n_person.yaml")  # build a new model from YAML
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolo11s_person.yaml").load("yolo11s.pt")  # build from YAML and transfer weights
+model = YOLO("yolo11n_person.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="CityPerson.yaml", 
+results = model.train(data="coco8.yaml", 
                       epochs=40, 
                       imgsz=640,
                       batch=8,
